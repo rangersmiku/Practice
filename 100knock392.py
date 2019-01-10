b027a4af8e4cd5b2bc2e5ee74a6a0f9eb05b9a57
Silky-Littleraisin <45849694+Silky-Littleraisin@users.noreply.github.com>
2019-01-10 23:29:39 +0900

    Add files via upload

---
+++ 100knock392.py
@@ -0,0 +1,40 @@
+parse_nek0()
+
+word_counter=Counter()
+for Line in neco_lines():
+    word_counter.update([morpheme['surface'] for morpheme in line )
+
+list_word=word_counter.most_common()
+
+
+counts = list(zip(*list_word))[1]
+
+
+fp= FontProperties(
+    fname='/usr/share/fonts/truetype/takao-gothic/TakaoGothic.ttf'
+    )
+
+
+plt.scatter(
+    range(1,len(counts) + 1),
+    counts
+)
+
+
+plt.scatter(
+    range(1,len(counts)+1),
+    counts
+)
+
+
+
+plt.xscale('log')
+plt.yscale('log')
+
+plt.title("39.Zopfnohosoku",fontproperties=fp)
+plt.xlabel('junni',fontproperties=fp)
+plt.ylabel('hinndo'fontproperties=fp)
+
+plt.grid(axis='both')
+
+plt.show()

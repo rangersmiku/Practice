c15e70961579349afa1198c274064c7e3066471f
Zehan Tian <45849694+Silky-Littleraisin@users.noreply.github.com>
2019-01-24 23:33:11 +0900

    Update 100-422.py

--- 100-422.py
+++ 100-422.py
@@ -22,9 +22,7 @@ def parse_neko():
 
 class Morph:
     '''
-    形態素クラス
-    表層形（surface）、基本形（base）、品詞（pos）、品詞細分類1（pos1）を
-    メンバー変数に持つ
+   
     '''
     def __init__(self, surface, base, pos, pos1):
         '''初期化'''

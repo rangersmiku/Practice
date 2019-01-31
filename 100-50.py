7fb03c045a390204f33953e5c2c77029efd324ba
Silky-Littleraisin <rangersmiku@gmail.com>
2019-01-31 13:14:53 +0900

    0131

--- 100-50.py
+++ 100-50.py
@@ -4,9 +4,35 @@ import re
 fname = 'nlp.txt'
 
 def nlp_lines():
-    
+
     ''' nlp を1文ずつ読み込むジェネレータ
     nlp.txtを順次読み込んで1文ずつ返す
-    ’’’
+    '''
 
     with open(fname) as lilnes:
+        pattern = re.compile(r'''
+        (^
+        .*?
+        [\.|;|:|\?|!]
+        )
+        \s
+        (
+            [A-Z].*
+        )
+        ''',re.MULTILINE+re.VERBOSE + re.DOTALL)
+
+        for line in lines:
+            line = line.strip()
+            while len(line) > 0:
+
+                match= pattern.match(line)
+                if match:
+
+                    yield match.group(1)
+                    line=match.group(2)
+
+                else:
+                    yield line
+                    line=''
+for line in nle_lines():
+    print (line)

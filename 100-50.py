4220df76ac375944573a04305f978f286a658d36
Silky-Littleraisin <rangersmiku@gmail.com>
2019-01-31 12:22:39 +0900

    0131

--- 100-50.py
+++ 100-50.py
@@ -1,11 +1,12 @@
 # coding utf-8
- import re
+import re
 
 fname = 'nlp.txt'
 
 def nlp_lines():
+    
     ''' nlp を1文ずつ読み込むジェネレータ
     nlp.txtを順次読み込んで1文ずつ返す
-
     ’’’
-    
+
+    with open(fname) as lilnes:

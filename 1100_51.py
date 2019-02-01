213426b62082d2a3948734ff9a1d89b3917aa6f1
Silky-Littleraisin <rangersmiku@gmail.com>
2019-02-01 19:08:24 +0900

    0201

---
+++ 1100_51.py
@@ -0,0 +1,57 @@
+#coding: utf-8
+import re
+
+fname='nlp.txt'
+
+def nle_lines():
+    ''' nlp.txtを1文ずつ読み込むジェネレータ
+    nlp.txtを順次読み込んで1文ずつ返す
+
+    戻り値：
+    1文の文字列
+    '''
+    with open(fname) as line:
+
+        pattern = re.compile(r'''
+        (
+            ^
+            .*?
+            [\.|\;|\:|\!]
+            )
+            \s
+            ([A-Z].*)
+        )
+        ''', re.MULITINE+ re.VERBOSE + re.DOTALL)
+
+        for line in lines:
+
+            line= line.strip()
+            while len(line)>0:
+
+                match = pattern.match(line)
+                if match:
+                    yield match.group(1)
+                    line=match.group(2)
+                else:
+
+                    yield line
+                    line=''
+
+def nlp_words():
+
+    '''nlp.txtを1単語ずつ返すジェネレータ
+    文の終わりでは空文字を返す。
+
+    戻り値：
+    1単語、ただし文の終わりでは空文字を返す
+    '''
+    for line in nlp_lines():
+
+        for word in line.strlit(' '):
+            yield word.rstrip('.,;:?!')
+
+        yield ''
+
+for word in nlp_words():
+
+    print(word)

1db09d6fd993d147dc8bb1f874f1d78593187781
Silky-Littleraisin <rangersmiku@gmail.com>
2019-02-01 19:18:54 +0900

    0201

--- 100_52.py
+++ 100_52.py
@@ -1,3 +1,9 @@
 #coding: utf-8
 import re
 import snowballstemmer
+
+stemmer=snowballstemmer.stemmer('english')
+for word in nlp_words():
+
+    print ('{}\t{}'.format(word,stemmer.stemWord(word)))
+    

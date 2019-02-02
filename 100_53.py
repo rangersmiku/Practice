bb6efb2af8c9ea662d009f1930e4b85ba6b8a4ca
Silky-Littleraisin <rangersmiku@gmail.com>
2019-02-02 18:31:45 +0900

    0202

--- 100_53.py
+++ 100_53.py
@@ -14,7 +14,7 @@ def parse_nlp():
 
         # StanfordCoreNLP実行、標準エラーはparse.outへ出力
         subprocess.run(
-            'java -cp "/usr/local/lib/stanford-corenlp-full-2016-10-31/*"'
+            'java -cp "/usr/local/lib/stanford-corenlp-full-2018-10-05/*"'
             ' -Xmx2g'
             ' edu.stanford.nlp.pipeline.StanfordCoreNLP'
             ' -annotators tokenize,ssplit,pos,lemma,ner,parse,dcoref'

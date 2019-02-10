29552258df7c4559dac2afdc3535295995166641
Silky-Littleraisin <rangersmiku@gmail.com>
2019-02-10 18:06:23 +0900

    0208

--- 1000_test.py
+++ 1000_test.py
@@ -9,14 +9,16 @@ fname_db = 'test_db'
 
 # LevelDBオープン
 db = leveldb.DB(bytes("/Users/silky/Documents/GitHub/Practice/level_test", "ascii"),create_if_missing=True)
-result=[]
+
 # valueが'Japan'のものを列挙
 clue = 'Japan'.encode()
 #result = [key.decode() for key,value in db if value == clue]
-for key in db.keys():
-    if db.get(key) == clue:
-        result.append(key)
-
+#for value in db:
+ #   if value[1] == clue:
+  #      result.append(value[0])
+for key, value in db:
+  print (key, value)
+  
 
 # 件数表示
 print('{}件'.format(len(result)))

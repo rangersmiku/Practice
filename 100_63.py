d4dc326f5705be490f439ae2739511d10fcbb011
Silky-Littleraisin <rangersmiku@gmail.com>
2019-02-10 17:42:32 +0900

    0208

--- 100_63.py
+++ 100_63.py
@@ -1,15 +1,19 @@
 # coding: utf-8
 import leveldb
+import gzip
+import json
+
+fname = 'artist.json.gz'
 
 fname_db = 'test_db'
 
 # LevelDBオープン
-db = leveldb.DB("/Users/silky/Documents/GitHub/Practice/level_test", "ascii",create_if_missing=True)
+db = leveldb.DB(bytes("/Users/silky/Documents/GitHub/Practice/level_test", "ascii"),create_if_missing=True)
 
 # valueが'Japan'のものを列挙
 clue = 'Japan'.encode()
 #result = [key.decode() for key,value in db if value == clue]
-for key,value in db :
+for key,value in db:
     if value == clue:
         result.append(key)
 

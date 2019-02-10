a13ce312729edf32920dc8f3853c922f37212a28
Silky-Littleraisin <rangersmiku@gmail.com>
2019-02-10 17:24:05 +0900

    0208

---
+++ 100_63.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import leveldb
+
+fname_db = 'test_db'
+
+# LevelDBオープン
+db = leveldb.DB(bytes("/Users/silky/Documents/GitHub/Practice/level_test", "ascii"),create_if_missing=True)
+
+# valueが'Japan'のものを列挙
+clue = 'Japan'.encode()
+#result = [key.decode() for key,value in db if value == clue]
+for key,value in db :
+    if value == clue:
+        result.append(key)
+
+
+# 件数表示
+print('{}件'.format(len(result)))
